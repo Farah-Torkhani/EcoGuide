9117d2bfd8960c7e284f3cb7a5c1c30410606ec4
Farah_Torkhani <79060004+Farah-Torkhani@users.noreply.github.com>
2023-11-05 14:27:53 +0100

    grid view and list view in recently booked

--- EcoGuide/recentlyBooked.swift
+++ EcoGuide/recentlyBooked.swift
@@ -1,11 +1,5 @@
-//
-//  recentlyBooked.swift
-//  EcoGuide
-//
-//  Created by Torkhani fara on 5/11/2023.
-//
-
 import SwiftUI
+
 struct ImageInfo1: Identifiable {
     let id = UUID()
     let imageName: String
@@ -15,86 +9,150 @@ struct ImageInfo1: Identifiable {
     let rating: String
 }
 
-struct recentlyBooked: View {
-    var image = ["guide1", "guide2", "guide3"]
-    @State private var imageInfoList: [ImageInfo1] = [
+struct RecentlyBooked: View {
+    @State private var isGridView = false
+    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2) // 2 columns
+
+    var imageInfoList: [ImageInfo1] = [
         ImageInfo1(imageName: "guide1", title: "Flen Fouleni", location: "tunis, France", price: "$29 / day", rating: "String"),
         ImageInfo1(imageName: "guide2", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),
-        ImageInfo1(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),// Add more image info items for each image
-        ImageInfo1(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),//
-        ImageInfo1(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "string"),// 
-     ]
+        ImageInfo1(imageName: "guide3", title: "Flen Fouleni", location: "Paris, France", price: "$29 / day", rating: "200 reviews"),
+        // Add more image info items for each image
+    ]
+
     var body: some View {
-        ScrollView{
+        ScrollView {
             Spacer()
             HStack {
                 Image(systemName: "arrow.left")
-
                     .font(.system(size: 22, weight: .semibold))
                 Text("Recently Booked")
                     .font(.system(size: 22, weight: .semibold))
                 Spacer()
                 HStack {
-                    
-                    Image(systemName: "doc.fill") //list view item
+                    Image(systemName: "doc.fill") // list view item
                         .font(.system(size: 24))
                         .foregroundColor(Color.blue)
-                    
-                    Image(systemName: "square.grid.2x2") // Grid icon
-                        .foregroundColor(.black).font(.system(size: 24))
-                    
-                    
-                    
-                    
-                }
-            }.padding(.trailing, 20)
-            
-            ForEach(imageInfoList) { imageInfo in
-                ZStack {
-                    Color(hex: "F3F8FE") // Set your desired background color here
-                        .frame(width: 350, height: 150)
-                        .cornerRadius(20)
-                    HStack(spacing: 5) {
-                        Image(imageInfo.imageName)
-                            .resizable()
-                            .frame(width: 100, height: 100).cornerRadius(10)
-                        VStack(alignment: .leading, spacing: 15) {
-                            Text(imageInfo.title)
-                                .font(.system(size: 23, weight: .semibold))
-                            Text(imageInfo.location)
-                                .font(.system(size: 16))
-                                .foregroundColor(Color.gray)
-                            HStack{
-                                Image(systemName: "star.fill")
-                                    .foregroundColor(.yellow)
-                                Text(imageInfo.rating)
-                                .font(.system(size: 15))}
-                        }
-                        .frame(maxWidth: .infinity) // Expand to fill the available space
-                        Spacer()
-                        VStack(alignment: .trailing, spacing: 15) {
-                            Text("$35")
-                                .font(.system(size: 23, weight: .semibold))
-                                .foregroundColor(Color.blue)
-                            Text("/ day")
-                            Image(systemName: "bookmark")
-                                .font(.system(size: 24))
-                                .foregroundColor(Color.black) //
-                            
-                        }
+                    Button(action: {
+                        isGridView.toggle()
+                    }) {
+                        Image(systemName: isGridView ? "list.dash" : "square.grid.2x2")
+                            .font(.system(size: 24))
+                            .foregroundColor(.black)
                     }
-                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
                 }
-                
-            }.padding(.trailing, 20)
-            
-        }.padding()
+            }
+            .padding(.trailing, 20)
+
+            if isGridView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(imageInfoList) { imageInfo in
+                        GridItemView(imageInfo: imageInfo)
+                    }
+                }
+                .padding(.trailing, 20)
+            } else {
+                ForEach(imageInfoList) { imageInfo in
+                    ListItemView(imageInfo: imageInfo)
+                }
+                .padding(.trailing, 20)
+            }
+        }
+        .padding()
     }
 }
 
+struct GridItemView: View {
+    var imageInfo: ImageInfo1
 
-struct recentlyBooked_Previews: PreviewProvider {
+    var body: some View {
+        ZStack {
+            Color(hex: "F3F8FE")
+                .frame(height: 250)
+                .cornerRadius(20)
+            VStack(spacing: 5) {
+                Image(imageInfo.imageName)
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .cornerRadius(10)
+                Text(imageInfo.title)
+                    .font(.system(size: 23, weight: .semibold))
+              
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                    Text(imageInfo.rating)
+                        .font(.system(size: 15))
+                }
+                Text(imageInfo.location)
+                    .font(.system(size: 16))
+                    .foregroundColor(Color.gray)
+                
+                HStack(spacing: 15) {
+                    Text("$35")
+                        .font(.system(size: 23, weight: .semibold))
+                        .foregroundColor(Color.blue)
+                    Text("/ day")
+                    Image(systemName: "bookmark")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color.black)
+                }
+            }
+            .frame(width:160)
+        
+          
+        }
+        .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15)
+        )
+    }
+}
+
+struct ListItemView: View {
+    var imageInfo: ImageInfo1
+
+    var body: some View {
+           ZStack {
+               Color(hex: "F3F8FE")
+                   .frame(height: 150).frame(width: 350)
+                   .cornerRadius(20)
+               HStack(spacing: 5) {
+                   Image(imageInfo.imageName)
+                       .resizable()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                VStack(alignment: .leading, spacing: 15) {
+                    Text(imageInfo.title)
+                        .font(.system(size: 23, weight: .semibold))
+                    Text(imageInfo.location)
+                        .font(.system(size: 16))
+                        .foregroundColor(Color.gray)
+                    HStack {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                        Text(imageInfo.rating)
+                            .font(.system(size: 15))
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                Spacer()
+                VStack(alignment: .trailing, spacing: 15) {
+                    Text("$35")
+                        .font(.system(size: 23, weight: .semibold))
+                        .foregroundColor(Color.blue)
+                    Text("/ day")
+                    Image(systemName: "bookmark")
+                        .font(.system(size: 24))
+                        .foregroundColor(Color.black)
+                }
+            }
+               .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+        }
+        .padding(.trailing, 4)
+    }
+}
+
+struct RecentlyBooked_Previews: PreviewProvider {
     static var previews: some View {
-        recentlyBooked()
+        RecentlyBooked()
     }
 }
